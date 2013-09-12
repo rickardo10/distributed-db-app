15e08db4d166e38ee2562695ff888432edc68aca
Ricardo Ocampo <rickardo10@gmail.com>
2013-09-12 12:50:53 -0500

    test4

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import cherrypy
 from cherrypy import wsgiserver
 import os
 
-class Root():
-	def application( environ, start_response ):
-		status = '200 OK'
-		response_headers = [ ('Content-type', 'text/plain')]
-		start_response( status, response_headers )
-		return ['Hello world!\n']
+def application( environ, start_response ):
+	status = '200 OK'
+	response_headers = [ ('Content-type', 'text/plain')]
+	start_response( status, response_headers )
+	return ['Hello world!\n']
 
 
 if __name__ == '__main__':
@@ -15,5 +14,5 @@ if __name__ == '__main__':
 	ip   = os.environ['OPENSHIFT_PYTHON_IP']
 	root = Root()
 	server = wsgiserver.CherryPyWSGIServer( ( ip, port ), 
-		root.application, server_name = 'www.cherrypy.example')
+		application, server_name = 'www.cherrypy.example')
 	server.start()
