b43b6bda5391ff92c5a09c72f274330c44ad64c8
Ricardo Ocampo <rickardo10@gmail.com>
2013-09-12 13:04:13 -0500

    test4

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ def application( environ, start_response ):
 	status = '200 OK'
 	response_headers = [ ('Content-type', 'text/plain')]
 	start_response( status, response_headers )
-	return ['<a href = "/set">Test 1</a>']
+	body = """
+	<html>
+		<body>
+			<a href = "/set">Test 1</a>
+		</body>
+	</html>
+	"""
+	return [body]
 
 def set( self ):
 	return ["prueba superada"]
