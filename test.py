2c084a16b34dab62cdf6c3eaa3192c3b0beac993
Ricardo Ocampo <rickardo10@gmail.com>
2013-09-12 08:59:19 -0500

    test1

--- test.py
+++ test.py
@@ -1,24 +1,30 @@
-import cherrypy
+from cherrypy import wsgiserver
+
 from pasteself.translogger import Translogger
 
 def application( environ, start_response ):
-	status = '200 OK'
-	response_headers = [('Content-type', 'text/plain')]
-	start_response( status,  response_headers )
-	return ['Hello world!\n']
+  status = '200 OK'
+  response_headers = [('Content-type', 'text/plain')]
+  start_response( status,  response_headers )
+  return ['Hello world!\n']
 
+def run_cherrypy_server(app, ip, port=8080):
+   from cherrypy import wsgiserver
+   server = wsgiserver.CherryPyWSGIServer(
+               (ip, port), app, server_name='www.cherrypy.example')
+   server.start()
 
 class Root:
-	def index( self ):
-		return "Primer Intento"
+  def index( self ):
+    return "Primer Intento"
 
 if __name__ == '__main__':
-	app = TransLogger( application )
-	conf = { '/': {'tools.wsgiapp.on': True,
-						'tools.wsgiapp.app': app,
-						'tools.gzipon': True}}
-	cherrypy.tree.mount(Root(), '/', config = conf )
-	cherrypy.server.quickstart()
-	cherrypy.engine.start()
+   ip   = os.environ['OPENSHIFT_PYTHON_IP']
+   port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+  app = TransLogger( application )
+  conf = { '/': {'tools.wsgiapp.on': True,
+            'tools.wsgiapp.app': app,
+            'tools.gzipon': True}}
+  run_cherrypy_server( app, ip, port )
 
 
