f3e6ce55e918891c177be0858cc11901ae633c0a
Ricardo Ocampo <rickardo10@gmail.com>
2013-09-12 13:00:27 -0500

    test4

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ def application( environ, start_response ):
 	status = '200 OK'
 	response_headers = [ ('Content-type', 'text/plain')]
 	start_response( status, response_headers )
-	return ['Hello world!\n']
+	return ['< a href = "/set">Test 1</a>']
+
+def set( self ):
+	return ["prueba superada"]
+set.expose = True
 
 
 if __name__ == '__main__':
-	port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
 	ip   = os.environ['OPENSHIFT_PYTHON_IP']
+	port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
 	server = wsgiserver.CherryPyWSGIServer( ( ip, port ), 
 		application, server_name = 'www.cherrypy.example')
 	server.start()
