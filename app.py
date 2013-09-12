71e88a54dc0efa2d3ede1df66a4c1f4b291b48d9
Ricardo Ocampo <rickardo10@gmail.com>
2013-09-12 12:12:43 -0500

    test5

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from cherrypy import wsgiserver
-import os
+import cherrypy
 
-def my_crazy_app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type','text/plain')]
-    start_response(status, response_headers)
-    return ['Hello world!']
+def application( environ, start_response ):
+	status = '200 OK'
+	response_headers = [ ('Content-type', 'text/plain')]
+	start_response( status, response_headers )
+	return ['Hello world!\n']
 
-ip   = os.environ['OPENSHIFT_PYTHON_IP']
-port = int(os.environ['OPENSHIFT_PYTHON_PORT'])				
 
-server = wsgiserver.CherryPyWSGIServer(
-            ( ip , port ), my_crazy_app,
-            server_name='www.cherrypy.example')
-server.start()
+class Root:
+	def index( self ):
+		return "Primer Intento"
+
+if __name__ == '__main__':
+	conf = { '/': { 'tools.wsgiapp.on': True,
+						 'tools.wsgiapp.app': application,
+						 'tools.gzip.on': True}}
+	cherrypy.tree.mount(Root(), '/', config = conf )
+	cherrypy.server.quickstart()
+	cherrypy.engine.start()
