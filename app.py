91e12dd2e90a9d27f27964db68e5384b5f42fc5f
Ricardo Ocampo <rickardo10@gmail.com>
2013-09-12 17:04:33 -0500

    test4

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
 import cherrypy
-from cherrypy import wsgiserver
-import os
 
-def application( environ, start_response ):
-	status = '200 OK'
-	body = """
-	<html>
-		<body>
-			<a href = "/set">Test 1</a>
-		</body>
-	</html>
-	"""
-	response_headers = [ ('Content-type', 'text/html')]
-	start_response( status, response_headers )
-	return [body]
-
-def set( self ):
-	return ["prueba superada"]
-
-d = wsgiserver.WSGIPathInfoDispatcher({'/': application, '/set': set})
+class Root(object):
+	def index(self):
+		return "Hello World"
+	main.expose = True
 
 if __name__ == '__main__':
-	ip   = os.environ['OPENSHIFT_PYTHON_IP']
-	port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
-	server = wsgiserver.CherryPyWSGIServer( ( ip, port ), 
-		d, server_name = 'www.cherrypy.example')
-	server.start()
+	http_conf = {'global': {'server.socket_port': port,
+									'server.socket_host': ip}}
+	cherrypy.config.update(http_conf)
+	cherrypy.engine.start()
