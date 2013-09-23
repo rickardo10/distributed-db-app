50e4c7103b64dd6c8d429071db7455867aede043
Ricardo Ocampo <rickardo10@gmail.com>
2013-09-23 03:33:18 -0500

    fixing css bug in restricted

--- app.py
+++ app.py
@@ -117,10 +117,10 @@ class HelloWorld(object):
 
 # Starts the webpage
 if __name__ == '__main__':
-	ip   = os.environ['OPENSHIFT_PYTHON_IP']
-	port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
-	#port = 8000
-	#ip = "127.0.0.1"
+	#ip   = os.environ['OPENSHIFT_PYTHON_IP']
+	#port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+	port = 8000
+	ip = "127.0.0.1"
 
 	http_conf = {'global': {'server.socket_port': port,
 									'server.socket_host': ip}}
@@ -135,7 +135,7 @@ if __name__ == '__main__':
 			  				 'tools.staticdir.dir': os.path.join(current_dir, 'static')}}
 	hello = HelloWorld()
 
-	cherrypy.tree.mount( hello.restricted, '/restricted1' ,config = conf)
+	cherrypy.tree.mount( hello.restricted ,config = conf)
 	cherrypy.quickstart( hello, "/", config = conf )
 
 #=========================================================================================
